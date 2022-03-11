61a18354ee5b2432c953240d679ae7cf510df2e9
Miles Wu <slion0725@gmail.com>
2022-03-11 10:30:57 +0800

    add:pages

--- pages/guest.vue
+++ pages/guest.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Index</h1>
+    <h1>Guest</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'GuestPage',
 }
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Index</h1>
+    <h1>Login</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'LoginPage',
 }
 </script>
