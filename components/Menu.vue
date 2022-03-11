5a345f3c5cd28457bd5e0a8a7463770e39da2b1e
Miles Wu <slion0725@gmail.com>
2022-03-11 10:36:07 +0800

    add

---
+++ components/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link to="/">Index</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/profile">Profile</router-link>
+    <router-link to="/guest">Guest</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuComponent',
+}
+</script>
