5b7bf1cc7265d725421eca8398067c569b40a078
Miles Wu <slion0725@gmail.com>
2022-03-11 13:42:28 +0800

    add:public

--- components/Menu.vue
+++ components/Menu.vue
@@ -4,6 +4,7 @@
     <router-link to="/login">Login</router-link>
     <router-link to="/profile">Profile</router-link>
     <router-link to="/guest">Guest</router-link>
+    <router-link to="/public">Public</router-link>
     <Logout />
 
     <div>{{ $auth.loggedIn }}</div>
---
+++ pages/public.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Public</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublicPage',
+  auth: false,
+}
+</script>
